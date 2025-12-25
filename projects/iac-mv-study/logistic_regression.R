40f779d9070ee55e8364d446a87bf33bc6c69303
Ousmane DIALLO <ousmanerabi12@gmail.com>
2025-12-24 19:55:58 -0600

    updated logistic regression

--- projects/iac-mv-study/logistic_regression.R
+++ projects/iac-mv-study/logistic_regression.R
@@ -29,19 +29,102 @@ final_cohort_v2 <- final_cohort %>%
     ),
     adm_type_grp = case_when(
       admission_type == "ELECTIVE" ~ "ELECTIVE",
-      admission_type %in% c("URGENT", "EMERGENCY") ~ "NON_ELECTIVE",
+      admission_type %in% c("URGENT", "EMERGENCY") ~ "NON ELECTIVE",
       TRUE ~ "OTHER"
+    ),
+    first_careunit_exclu = ifelse(first_careunit=="CSRU", 1, 0),
+    icu_type = case_when(
+      first_careunit == "MICU" ~ "MICU",
+      first_careunit %in% c("SICU", "TSICU") ~ "SURGICAL",
+      first_careunit == "CCU" ~ "CARDIAC",
+      TRUE ~ "OTHER"
+    ),
+    iac_exposed_re = ifelse(iac_exposed==0, "No-IAC", "IAC")
+  )
+
+# Excluded the patients admitted in CSRU – Cardiac Surgery Recovery Unit
+
+final_cohort_v3 = final_cohort_v2 %>%
+  dplyr::filter(first_careunit_exclu == 0,
+                vaso_24h==0) %>%
+  dplyr::select(subject_id, hadm_id, icustay_id, intime, outtime, icu_los_hours,
+                age, mv_start, death_28d, iac_exposed, vaso_24h, gender,
+                insurance_grp, icu_type, charlson_score, adm_type_grp,iac_exposed_re, ethnicity_grp) %>%
+  dplyr::mutate(
+    dplyr::across(
+      c(death_28d, iac_exposed_re, vaso_24h, gender, insurance_grp, icu_type, adm_type_grp,
+        ethnicity_grp),
+      as.factor
     )
   )
 
-
 table2 <-
   tbl_summary(
-    full_cohort_data,
-    include = c(age,),
-    by = trt, # split table by group
+    final_cohort_v3,
+    include = c(age,gender, insurance_grp, icu_type, charlson_score, adm_type_grp, death_28d,
+                ethnicity_grp),
+    by = iac_exposed_re, # split table by group
     missing = "no" # don't list missing data separately
-  ) |> 
+  ,
+  label = list(age = "Age (years)", gender="Sex",
+               charlson_idex="Charlson comorbidity index",
+               icu_type="ICU type",
+               #vaso_24h="Vasopressor use (first 24h)",
+               ethnicity_grp = "Ethnic group",
+               insurance_grp = "Insurance",
+               adm_type_grp = "Admission type",
+               death_28d = "28-day mortality"
+               )) |> 
   add_n() |> # add column with total number of non-missing observations
   add_p() |> # test for a difference between groups
-  modify_header(l
+  modify_header(label = "**Variable**") |> # update the column header
+  bold_labels() 
+
+##
+
+
+tab1_smd <- final_cohort_v3 %>% dplyr::select(age, gender,charlson_score,
+                                              icu_type,
+                            insurance_grp, adm_type_grp, death_28d, iac_exposed_re,
+                            ethnicity_grp) %>%
+  tbl_summary(
+    by = iac_exposed_re,
+    label = list(age = "Age (years)", 
+                 gender="Sex",
+                 charlson_idex="Charlson comorbidity index",
+                 icu_type="ICU type",
+                 #vaso_24h="Vasopressor use (first 24h)",
+                 insurance_grp = "Insurance",
+                 adm_type_grp = "Admission type",
+                 ethnicity_grp = "Ethnic group",
+                 death_28d = "28-day mortality"),
+    statistic = list(
+      all_continuous() ~ "{median} ({p25}, {p75})",
+      all_categorical() ~ "{n} ({p}%)"
+    ),
+    missing = "no"
+  ) %>%
+  add_difference(
+    test = list(
+      all_continuous() ~ "smd",
+      all_categorical() ~ "smd"
+    )
+  ) %>%
+  modify_header(
+    estimate ~ "**SMD**"
+  ) %>%
+  bold_labels()
+
+### Logistic regression
+
+fit <- glm(death_28d~iac_exposed_re+age+gender+charlson_score+icu_type+insurance_grp+adm_type_grp+ethnicity_grp, family = binomial, data = final_cohort_v3)
+
+### Display the model fit
+summary(fit)
+
+## Odds ratio
+questionr::odds.ratio(fit)
+
+## Forest plot
+
+
